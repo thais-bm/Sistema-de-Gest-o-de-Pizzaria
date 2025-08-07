7afaf82027fd170d7cb256e2af49014f3458276c
Thais Carolina Braga de Magalhães <tcbdma.snf24@uea.edu.br>
2025-08-07 12:19:46 -0400

    Initialialized components + added model

---
+++ src/components/PizzaCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PizzaCard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PizzaCard
---
+++ src/pages/Cardapio.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Cardapio = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Cardapio
